2a8a994891cfd676276651f2b5742ad26d685eee
alstjr13 <alstjr6030@naver.com>
2024-08-04 23:09:10 -0700

    description

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 import numpy as np
 import math
 
+# Project with BERT large, Bigbird-RoBERTa-large, Longformer
 
-
+# TODO:
+# Make 5 figures of epoch = 1,2,3 and test of the following criterias:
+# 1. Accuracy
+# 2. Precision
+# 3. Recall
+# 4. F1 Score
+# 5. AUC
+# Epoch 1,2,3 test
 
 if __name__ == "__main__":
     print("hello world")
